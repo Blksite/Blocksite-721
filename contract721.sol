161a589aaf36979bfd97db057201f114ea808bad
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-08-06 09:40:41 +1000

    Create contract721.sol

---
+++ contract721.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/access/Ownable.sol";
+
+contract Blocksite721 is ERC721 {
+  mapping(address => uint) tokens;
+  function approval(address _owner, address _approved,uint _tokenId){
+		require(tokens[_owner]==_tokenId);
+		tokens[_approved]=_tokenId;
+	}
+	function transfer(address _to, uint _amount) public payable{
+		require(_amount <= tokens[msg.sender]);
+		tokens[msg.sender]-=_amount;
+		tokens[_to]+=_amount;
+	}
+	function balanceOf(address _owner) public view returns (uint){
+		return tokens[_owner];
+	}
+	function ownerOf(uint _tokenId) public view returns(address){
+		return tokens[_id].address;
+	}
+	function TransferFrom(address _from, address _to, uint _tokenId) payable{
+		require(tokens[_from]==_tokenId);
+		tokens[_from]=0;
+		tokens[_to]=_tokenId;
+	}
+	function approve(address _approved, uint _tokenId) payable{
+		require(tokens[msg.sender]==_tokenId);
+		tokens[_approved]=_tokenId;
+	}
+	function mint(address _to, uint _tokenId,) public{
+		tokens[_to] = 'mytoken '+str(uint(blockhash(block.number - 1)));
+	}
