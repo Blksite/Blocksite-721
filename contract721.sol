8943f550528e51d413e3eed70eb969cd5f26fedf
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-08-07 20:46:55 +1000

    Update contract721.sol

--- contract721.sol
+++ contract721.sol
@@ -1,26 +1,15 @@
-// contracts/Blocksite721.sol
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-
+pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-
-contract GameItem is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    constructor() public ERC721("Blocksite", "BLK") {}
-
-    function mint(address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
-    }
+contract SimpleCollectible is ERC721 {
+  uint256 public tokenCounter;
+  constructor () public ERC721 ("Blocksite", "BLK"){
+    tokenCounter = 0;
+  }
+  function mint(string memory tokenURI) public returns (uint256) {
+    uint256 newItemId = tokenCounter;
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+    tokenCounter = tokenCounter + 1;
+    return newItemId;
+  }
 }
